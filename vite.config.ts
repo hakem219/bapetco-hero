786611d777e08c34056c6ad4b415d1e764fd4d8c
hakem219 <a.hakem@hotmail.com>
2025-12-04 01:38:04 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glsl'],
-   base:'/bapetco-new-web/'
+   base:'/bapetco-hero/'
 })
